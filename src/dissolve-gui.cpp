7ac01d328b4906e422d56b3a717a100690d7aabc
EthanDevlinSTFC <ethan.devlin@stfc.ac.uk>
2021-08-16 12:03:45 +0100

    fixed message window

--- src/dissolve-gui.cpp
+++ src/dissolve-gui.cpp
@@ -58,7 +58,7 @@ int main(int args, char **argv)
                      Version::info());
     Messenger::print("Source repository: {}.\n", Version::repoUrl());
     Messenger::print("Dissolve comes with ABSOLUTELY NO WARRANTY.\n");
-    Messenger::print("This is test software, and you are welcome to redistribute it under certain conditions.\n");
+    Messenger::print("This is free software, and you are welcome to redistribute it under certain conditions.\n");
     Messenger::print("For more details read the GPL at <http://www.gnu.org/copyleft/gpl.html>.\n");
 
     // If an input file was specified, load it here
